9c2fbe3c176cee76d9689b5611713a1ef7191d1f
ahmedhossain29 <hossainahmad76@gmail.com>
2021-06-19 17:19:03 +0600

    Theme

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+
+  const HomeDetailPage({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: context.canvasColor,
+      bottomNavigationBar: Container(
+        color: context.cardColor,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.red800.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    context.theme.buttonColor,
+                  ),
+                  shape: MaterialStateProperty.all(
+                    StadiumBorder(),
+                  )),
+              child: "Add to Cart".text.make(),
+            ).wh(120, 45)
+          ],
+        ).p32(),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context),
+            Expanded(
+                child: VxArc(
+              height: 30.0,
+              arcType: VxArcType.CONVEY,
+              edge: VxEdge.TOP,
+              child: Container(
+                color: context.cardColor,
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4.bold
+                        .color(context.accentColor)
+                        .make(),
+                    catalog.desc.text.xl.caption(context).make(),
+                    10.heightBox,
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                        .text
+                        .make()
+                        .p16()
+                  ],
+                ).py64(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-      //primarySwatch: Colors.blue,
+      primarySwatch: Colors.blue,
       fontFamily: GoogleFonts.lato().fontFamily,
+      cardColor: Colors.white,
+      canvasColor: creamColor,
+      buttonColor: darkColor,
+      accentColor: darkColor,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -14,10 +19,23 @@ class MyTheme {
       primaryTextTheme: GoogleFonts.latoTextTheme());
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-        brightness: Brightness.dark,
-      );
+      brightness: Brightness.dark,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      cardColor: Colors.black,
+      canvasColor: darkcremColor,
+      buttonColor: lightBluishColor,
+      accentColor: Colors.white,
+      appBarTheme: AppBarTheme(
+        color: Colors.black,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.white),
+        textTheme: Theme.of(context).textTheme,
+      ),
+      primaryTextTheme: GoogleFonts.latoTextTheme());
 
   //Colors
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkColor = Color(0xff403b58);
+  static Color darkcremColor = Vx.gray900;
+  static Color lightBluishColor = Vx.indigo500;
 }
